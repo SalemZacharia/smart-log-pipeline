225151931c522f91e1e0c5a18e1d86ac7a26706e
SalemZacharia <99037612+SalemZacharia@users.noreply.github.com>
2025-07-10 21:00:51 +0200

    all controllers work, pipeline completed

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from transformation import transform_logs
 from enrichment import enrich_logs
 from export import export_logs
 
-# Fichiers d'entrée/sortie
-input_path = "./data/raw"
-parsed_path = "./data/processed/parsed_logs.csv"
-transformed_path = "./data/processed/transformed_logs.csv"
+if __name__ == "__main__":
+    input_path = "./data/raw"
+    parsed_path = "./data/processed/parsed_logs.csv"
+    transformed_path = "./data/processed/transformed_logs.csv"
+    enriched_path = "./data/processed/enriched_logs.csv"
 
-# Pipeline complet
-parse_logs(input_path, parsed_path)
-transform_logs(parsed_path, transformed_path)
-enrich_logs(transformed_path)
-export_logs(transformed_path)
+    parse_logs(input_path, parsed_path)
 
-print("✅ Pipeline complet terminé.")
+    transform_logs(parsed_path, transformed_path)
+
+    enrich_logs(transformed_path, enriched_path)
+
+    export_logs(enriched_path)
+
+    print("✅ Pipeline complete!")
